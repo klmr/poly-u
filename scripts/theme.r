3898032748a12e5b5d552260bc6a723e8dea8741
Konrad Rudolph <konrad.rudolph@gmail.com>
2016-08-13 18:54:36 +0100

    Make theme more minimal

--- scripts/theme.r
+++ scripts/theme.r
@@ -4,8 +4,8 @@ theme = theme_bw() +
     theme(strip.background = element_blank(),
           panel.grid.minor = element_blank(),
           rect = element_blank(),
-          axis.line.x = element_line(size = 0.5),
-          axis.line.y = element_line(size = 0.5),
+          axis.line.x = element_line('grey50', size = 0.5),
+          axis.line.y = element_line('grey50', size = 0.5),
           legend.title = element_blank())
 
 theme_set(theme)
